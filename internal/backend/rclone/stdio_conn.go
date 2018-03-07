717195582ebf4df7be75dee1c97368cfdd2dd552
Alexander Neumann <alexander@bumpern.de>
2018-03-07 22:04:44 +0100

    Make StdioConn compile with Go < 1.10

--- internal/backend/rclone/stdio_conn.go
+++ internal/backend/rclone/stdio_conn.go
@@ -5,7 +5,6 @@ import (
 	"net"
 	"os"
 	"os/exec"
-	"time"
 )
 
 // StdioConn implements a net.Conn via stdin/stdout.
@@ -55,26 +54,6 @@ func (s *StdioConn) LocalAddr() net.Addr {
 // RemoteAddr returns nil.
 func (s *StdioConn) RemoteAddr() net.Addr {
 	return Addr{}
-}
-
-// SetDeadline sets the read/write deadline.
-func (s *StdioConn) SetDeadline(t time.Time) error {
-	err1 := s.stdin.SetReadDeadline(t)
-	err2 := s.stdout.SetWriteDeadline(t)
-	if err1 != nil {
-		return err1
-	}
-	return err2
-}
-
-// SetReadDeadline sets the read/write deadline.
-func (s *StdioConn) SetReadDeadline(t time.Time) error {
-	return s.stdin.SetReadDeadline(t)
-}
-
-// SetWriteDeadline sets the read/write deadline.
-func (s *StdioConn) SetWriteDeadline(t time.Time) error {
-	return s.stdout.SetWriteDeadline(t)
 }
 
 // make sure StdioConn implements net.Conn
